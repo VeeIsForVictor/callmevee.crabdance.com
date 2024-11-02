3a08672adb76480446d2e0ac806e21b7a7b2fe3b
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-11-02 16:02:13 +0800

    feat: prefer world over there

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
-		<h1 class="h1">Hello, there!</h1>
+		<h1 class="h1">Hello, world!</h1>
 		<p>I'm Victor and this is my personal website's WIP front page. The fact you can see this means my web server is in a rudimentary state and should be ready eventually. Cheers!</p>
 	</div>
 </div>
