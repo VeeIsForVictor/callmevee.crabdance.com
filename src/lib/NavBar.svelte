743e214e27fb07f74bdcf7032ae62eb0e771d3fe
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-11-02 17:44:24 +0800

    feat: style navbar

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -1,5 +1,18 @@
-<nav>
-    <nav>
-        <h3>CallMeVee</h3>
-    </nav>
-</nav>
+<script>
+	import { LightSwitch } from "@skeletonlabs/skeleton";
+
+</script>
+
+<div>
+    <div class="flex items-center justify-between rounded-xl border-2 border-surface-700 shadow-lg backdrop-blur-xl lg:mx-24 p-2">
+        <h4 class="h4">CallMeVee</h4>
+        <nav>
+            <ul class="flex flex-row justify-around gap-20 -translate-x-1/4">
+                <li><h5>Projects</h5></li>
+                <li><h5>Blog</h5></li>
+                <li><h5>Me</h5></li>
+            </ul>
+        </nav>
+        <LightSwitch />
+    </div>
+</div>
