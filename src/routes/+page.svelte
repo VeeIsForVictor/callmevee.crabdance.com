03509b73c2d56a8557e15905c51f057b175ab0f0
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-11-02 18:07:11 +0800

    feat: use prose class in text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,12 @@
 	<title>CallMeVee</title>
 </head>
 
-<div class="container h-full mx-auto flex justify-center items-center">
+<div class="container h-full mx-auto flex justify-center items-center prose dark:prose-invert">
 	<div class="space-y-5">
 		<h1 class="h1">Hello, world!</h1>
 		<p>
 			I'm Victor and this is my personal website's WIP front page. The fact you can see this means
 			my web server is in a rudimentary state and should be ready eventually. Cheers!
 		</p>
-		<p>Frontend test here</p>
 	</div>
 </div>
