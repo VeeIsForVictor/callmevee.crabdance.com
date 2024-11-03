ff483e69d6fa1266b4dc00b2394d0a0f6042afcb
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-11-03 18:55:19 +0800

    feat: create blog model

---
+++ src/lib/models/blog.ts
@@ -0,0 +1,11 @@
+import { cleanHtml, validateDateCreated, validateDateUpdated } from "$lib/model-utilities";
+import { object, pipe, string, type InferOutput } from "valibot";
+
+export const Blog = object({
+    date_created: validateDateCreated,
+    date_updated: validateDateUpdated,
+    headline: string(),
+    subheadline: pipe(string(), cleanHtml)
+})
+
+export type Blog = InferOutput<typeof Blog>;
