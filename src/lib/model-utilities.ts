a19772f8cc8c5ee75c9f050b4c5d1fcf99ead02b
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-11-03 18:53:12 +0800

    feat: add model helpers for dates

--- src/lib/model-utilities.ts
+++ src/lib/model-utilities.ts
@@ -1,4 +1,9 @@
 import sanitize from 'sanitize-html';
-import { transform } from 'valibot';
+import { isoTimestamp, nullable, pipe, string, transform } from 'valibot';
 
+// valibot transform for cleaning html
 export const cleanHtml = transform((input: string) => sanitize(input));
+
+// validation shortcuts for dates (created and updated), note that dateUpdateds can be null
+export const validateDateCreated = pipe(string(), isoTimestamp());
+export const validateDateUpdated = nullable(pipe(string(), isoTimestamp()));
