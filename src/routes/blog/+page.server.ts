b54da7c4271c69905473d6cfd11f8ab23c794b6c
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-11-03 23:56:12 +0800

    feat: request blog_posts_tags from directus

--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -1,6 +1,7 @@
 import getDirectusInstance from '$lib/directus';
 import { Blog } from '$lib/models/blog.js';
 import { BlogPosts } from '$lib/models/blog_post.js';
+import { BlogPostsTags } from '$lib/models/blog_posts_tags.js';
 import { Tags } from '$lib/models/tag.js';
 import { readItems, readSingleton } from '@directus/sdk';
 import { parse } from 'valibot';
@@ -8,8 +9,9 @@ import { parse } from 'valibot';
 export async function load({ fetch }) {
 	const directus = getDirectusInstance(fetch);
 	return {
-		blogPosts: parse(BlogPosts, await directus.request(readItems('blog_posts'))),
 		blog: parse(Blog, await directus.request(readSingleton('blog'))),
+		blogPosts: parse(BlogPosts, await directus.request(readItems('blog_posts'))),
+		blogPostsTags: parse(BlogPostsTags, await directus.request(readItems('blog_posts_tags'))),
 		tags: parse(Tags, await directus.request(readItems('tags')))
 	};
 }
