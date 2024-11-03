05e79003ea82261c54105aaca7c5510a0a703950
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-11-03 19:02:52 +0800

    feat: define root schema

---
+++ src/lib/models/schema.ts
@@ -0,0 +1,12 @@
+import { object, type InferOutput } from "valibot";
+import { BlogPosts } from "./blog_post";
+import { Blog } from "./blog";
+import { Tags } from "./tag";
+
+export const Schema = object({
+    blog: Blog,
+    blog_posts: BlogPosts,
+    tags: Tags
+})
+
+export type Schema = InferOutput<typeof Schema>;
