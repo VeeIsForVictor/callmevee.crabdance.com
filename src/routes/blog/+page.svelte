556ee9307acfbe2b64e2b0ffad4b26c06ee29f66
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-11-03 21:08:25 +0800

    feat: create filler content

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -4,6 +4,23 @@
     $: ({ blog, blogPosts, tags } = data);
 </script>
 
-<div>
-    <h3 class="h3">{blog.headline}</h3>
+<div class="flex flex-col">
+    <div class="flex flex-col items-center">
+        <h1 class="h1">{blog.headline}</h1>
+        <p>{@html blog.subheadline}</p>
+    </div>
+    <hr>
+    <div class="flex container grow">
+        <div class="flex flex-row align-center gap-4">
+            {#each blogPosts as {title, slug}}
+                <a href="./{slug}">
+                    <div class="card w-96">
+                        <header class="card-header">
+                            <h4 class="h4 p-4">{title}</h4>
+                        </header>
+                    </div>
+                </a>
+            {/each}
+        </div>
+    </div>
 </div>
