84a448632a46f24715703041b7119a68e0dc21c3
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-11-06 11:12:00 +0800

    docs: document layout for blog landing page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -34,11 +34,14 @@
 </svelte:head>
 
 <div class="h-full w-full flex flex-col justify-left items-center text-justify gap-16 py-20">
+	<!-- Hero -->
 	<div class="flex flex-col items-center prose dark:prose-invert">
 		<h1 class="h1">{blog.headline}</h1>
 		<p>{@html blog.subheadline}</p>
 	</div>
+
 	<div class="flex flex-col h-full w-full shadow-md p-8 bg-surface-0 dark:bg-surface-900 rounded-lg gap-8">
+		<!-- Featureds Carousel -->
 		<h3 class="h3 prose dark:prose-invert">
 			<select class="select w-40" style="text-transform: capitalize" bind:value={featuredTag} on:change={() => {featuredTag = featuredTag}}>
 				{#each tags as {tag_name}}
@@ -47,5 +50,8 @@
 			</select> Posts
 		</h3>
 		<BlogCarousel cards={blogCards} />
+		
+		<!-- Posts Grid -->
+
 	</div>
 </div>
