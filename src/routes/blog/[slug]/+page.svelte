1358f3f0ba2b4ed57881e682b9c43e6f51b3a074
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-11-07 21:56:03 +0800

    feat: add hero component to blog posts

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Hero from '$lib/components/Hero.svelte';
+
     export let data;
 </script>
 
@@ -7,9 +9,7 @@
 </svelte:head>
 
 <div class="h-full w-full flex flex-col justify-left items-center text-justify gap-16 py-20">
-	<div class="flex flex-col items-center prose dark:prose-invert">
-		<h3 class="h3">{data.title}</h3>
-	</div>
+	<Hero heroHeadline={data.title} heroSubheadline="" heroBackgroundHeight={300} heroBackgroundWidth={1200}/>
 	<div class="flex flex-col h-full w-full shadow-md p-8 bg-surface-900 rounded-lg gap-4">
 		<p class="prose dark:prose-invert">{@html data.content}</p>
 	</div>
