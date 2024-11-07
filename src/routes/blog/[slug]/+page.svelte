ae86b553476949f359fd35873eb8532df7c3b3cc
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-11-07 22:19:07 +0800

    fix: change max-width for prose-d element in blog posts

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -10,7 +10,7 @@
 
 <div class="h-full w-full flex flex-col justify-left items-center text-justify gap-16 py-20">
 	<Hero heroHeadline={data.title} heroSubheadline="" heroBackgroundHeight={300} heroBackgroundWidth={1200}/>
-	<div class="flex flex-col h-full w-full shadow-md p-8 bg-surface-100 dark:bg-surface-900 rounded-lg gap-4">
-		<p class="prose dark:prose-invert">{@html data.content}</p>
+	<div class="flex flex-col h-full w-full !max-w-none shadow-md p-8 bg-surface-100 dark:bg-surface-900 rounded-lg gap-4 prose dark:prose-invert">
+		{@html data.content}
 	</div>
 </div>
