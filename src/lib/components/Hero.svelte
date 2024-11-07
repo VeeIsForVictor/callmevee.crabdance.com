4903c43354b0d265250352e2a1a4d6756c6d2d35
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-11-07 22:04:03 +0800

    feat: create animated hero background when no background url provided

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -12,9 +12,34 @@
 </script>
 
 <div class="flex w-full justify-center items-center bg-white dark:bg-slate-800 rounded-3xl" style="height: {heroBackgroundHeight}px; width: {heroBackgroundWidth}px; z-index: -10;">
-    <div class="opacity-30 rounded-3xl shadow-lg" style="position: absolute; background-image: url({heroBackgroundURL}); height: {heroBackgroundHeight}px; width: {heroBackgroundWidth}px; z-index: -9;"/>
+    {#if heroBackgroundURL}
+        <div class="opacity-30 rounded-3xl shadow-lg" style="position: absolute; background-image: url({heroBackgroundURL}); height: {heroBackgroundHeight}px; width: {heroBackgroundWidth}px; z-index: -9;"/>
+    {:else}
+        <div class="animated-background bg-gradient-to-r from-slate-400 via-blue-400 to-surface-500 dark:from-slate-800 dark:via-blue-950 dark:to-surface-900 rounded-3xl shadow-lg" style="position: absolute; height: {heroBackgroundHeight}px; width: {heroBackgroundWidth}px; z-index: -9;"/>
+    {/if}
     <div class="flex flex-col items-center justify-center prose dark:prose-invert">
         <h1 class="h1">{heroHeadline}</h1>
         <p>{@html heroSubheadline}</p>
     </div>
 </div>
+
+<style>
+    .animated-background {
+        background-size: 400%;
+
+        -webkit-animation: animation 6s ease infinite;
+        -moz-animation: animation 6s ease infinite;
+        animation: animation 6s ease infinite;
+    }
+
+    @keyframes animation {
+        0%,
+        100% {
+            background-position: 0% 50%;
+        }
+        
+        50% {
+            background-position: 100% 50%;
+        }
+    }
+</style>
