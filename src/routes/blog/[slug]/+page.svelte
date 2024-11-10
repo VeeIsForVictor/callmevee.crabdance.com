365ceabd59048cc56042b8fb6d0f9a91c61db7ff
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-11-11 02:53:50 +0800

    feat: add date displays to blog posts

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,17 +1,31 @@
 <script lang="ts">
+	import { assert } from '$lib/assert';
 	import Hero from '$lib/components/Hero.svelte';
 	import type { BlogPost } from '$lib/models/blog_post';
 	
     export let data: BlogPost;
+	let { title, post_content, date_created, date_updated } = data;
+
+	assert(date_updated !== null)
 </script>
 
 <svelte:head>
-	<title>{data.title}</title>
+	<title>{title}</title>
 </svelte:head>
 
 <div class="h-full w-full flex flex-col justify-left items-center text-justify gap-16 py-20">
-	<Hero heroHeadline={data.title} heroSubheadline="" heroBackgroundHeight={300} heroBackgroundWidth={1200}/>
-	<div class="flex flex-col h-full w-full !max-w-none shadow-md p-8 bg-surface-100 dark:bg-surface-900 rounded-lg gap-4">
-		{@html data.post_content}
+	<Hero heroHeadline={title} heroSubheadline="" heroBackgroundHeight={300} heroBackgroundWidth={1200}/>
+	<div class="flex flex-row gap-4">
+		<div class="w-3/12">
+			<div class="w-full prose dark:prose-invert bg-surface-100 dark:bg-surface-900 rounded-lg p-8">
+				<h4>Date created:</h4>
+				<h5>{new Date(date_created).toLocaleDateString()}</h5>
+				<h4>Date updated:</h4>
+				<h5>{new Date(date_updated).toLocaleDateString()}</h5>
+			</div>
+		</div>
+		<div class="flex flex-col h-full w-9/12 !max-w-none shadow-md p-8 bg-surface-100 dark:bg-surface-900 rounded-lg gap-4">
+			{@html post_content}
+		</div>
 	</div>
 </div>
