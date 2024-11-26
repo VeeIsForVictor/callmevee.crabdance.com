3239fc25ce5d46ff76a23872e2747c2853a71d23
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-11-26 21:44:06 +0800

    fix: make hero component responsive

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -18,18 +18,26 @@
         heroSubheadline
     }: Props = $props();
 
-	let heroBackgroundURL = heroBackgroundRawId ? `${PUBLIC_APIURL}/assets/${heroBackgroundRawId}?width=${heroBackgroundWidth}&height=${heroBackgroundHeight}&fit=cover&format=webp` : null;
+    let outerWidth = $state(0);
+    let heroScalingFactor = $derived(heroBackgroundWidth < outerWidth ? 1 : 0.3);
+
+    let actualBackgroundHeight = $derived(heroBackgroundHeight * heroScalingFactor);
+    let actualBackgroundWidth = $derived(heroBackgroundWidth * heroScalingFactor);
+
+	let heroBackgroundURL = $derived(heroBackgroundRawId ? `${PUBLIC_APIURL}/assets/${heroBackgroundRawId}?width=${actualBackgroundWidth}&height=${actualBackgroundHeight}&fit=cover&format=webp` : null);
 </script>
 
-<div class="flex w-full justify-center items-center bg-white dark:bg-slate-800 rounded-3xl" style="height: {heroBackgroundHeight}px; width: {heroBackgroundWidth}px; z-index: -10;">
+<svelte:window bind:outerWidth/>
+
+<div class="flex w-full justify-center items-center bg-white dark:bg-slate-800 rounded-3xl" style="height: {actualBackgroundHeight}px; width: {actualBackgroundWidth}px; z-index: -10;">
     {#if heroBackgroundURL}
-        <div class="opacity-30 rounded-3xl shadow-lg" style="position: absolute; background-image: url({heroBackgroundURL}); height: {heroBackgroundHeight}px; width: {heroBackgroundWidth}px; z-index: -9;"></div>
+        <div class="opacity-30 rounded-3xl shadow-lg" style="position: absolute; background-image: url({heroBackgroundURL}); height: {actualBackgroundHeight}px; width: {actualBackgroundWidth}px; z-index: -9;"></div>
     {:else}
         <div class="animated-background bg-gradient-to-r from-slate-100 via-blue-400 to-surface-500 dark:from-slate-800 dark:via-blue-950 dark:to-surface-900 rounded-3xl shadow-lg" style="position: absolute; height: {heroBackgroundHeight}px; width: {heroBackgroundWidth}px; z-index: -9;"></div>
     {/if}
     <div class="flex flex-col items-center justify-center prose dark:prose-invert">
-        <h1 class="h1">{heroHeadline}</h1>
-        <p>{@html heroSubheadline}</p>
+        <h1 class="lg:h1 h2 text-center">{heroHeadline}</h1>
+        <p class="lg:text-lg text-sm text-center">{@html heroSubheadline}</p>
     </div>
 </div>
 
