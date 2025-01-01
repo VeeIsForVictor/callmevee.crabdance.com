efb57504128c914092c94781593ccd28250e1cff
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-01-01 23:19:10 +0800

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+    let { status, error } = $page;
+</script>
+
+<h3 class="h3">
+    <strong>{status}:</strong>
+    {#if error !== null}
+        {error.message}
+    {/if}
+</h3>
