241a956bef4190d7e15d006f336cef196dc8098c
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-01-02 23:56:27 +0800

    feat: add placeholder for the rest of the Work page

--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -6,6 +6,13 @@
 	<title>CallMeVee</title>
 </svelte:head>
 
-<div class="flex flex-col w-full">
-	<SkillSummary />
+<div class="flex flex-col w-full space-y-32 items-center">
+	<SkillSummary/>
+
+	<div class="space-y-5 prose dark:prose-invert w-1/2">
+		<h1 class="h1">Hi there!</h1>
+		<p>
+			This page is a work in progress, check back later when this page looks cooler!
+		</p>
+	</div>
 </div>
