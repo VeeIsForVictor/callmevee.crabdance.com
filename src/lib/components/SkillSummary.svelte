031197a56e135315d6a32b68ddc7d46296bed4e0
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-01-02 23:50:58 +0800

    feat: add SkillSummary component

---
+++ src/lib/components/SkillSummary.svelte
@@ -0,0 +1,3 @@
+<div class="flex w-full justify-center items-center bg-white dark:bg-slate-800 rounded-3xl max-w-none p-10">
+    Skill Summary
+</div>
--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -1,10 +1,11 @@
+<script>
+	import SkillSummary from "$lib/components/SkillSummary.svelte";
+
+</script>
 <svelte:head>
 	<title>CallMeVee</title>
 </svelte:head>
 
-<div class="space-y-5 prose dark:prose-invert">
-	<h1 class="h1">Hi there!</h1>
-	<p>
-		This page is a work in progress, check back later when this page looks cooler!
-	</p>
+<div class="flex flex-col w-full">
+	<SkillSummary />
 </div>
