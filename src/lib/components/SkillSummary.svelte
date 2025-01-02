993b181e93b9e5a5f036a4114baf3164704e108c
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-01-02 23:54:39 +0800

    feat: add placeholders for SkillSummary

--- src/lib/components/SkillSummary.svelte
+++ src/lib/components/SkillSummary.svelte
@@ -1,3 +1,7 @@
-<div class="flex w-full justify-center items-center bg-white dark:bg-slate-800 rounded-3xl max-w-none p-10">
-    Skill Summary
+<div class="flex prose dark:prose-invert w-full justify-center items-center bg-white dark:bg-slate-800 rounded-3xl max-w-none p-10">
+    <div class="grid grid-cols-3 gap-10 justify-items-center prose-headings:my-4">
+        <div><h3>Languages</h3></div>
+        <div><h3>Libraries and Frameworks</h3></div>
+        <div><h3>Technologies</h3></div>
+    </div>
 </div>
