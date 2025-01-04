feaa3c1d7ad9fe15351150eb9e7b01665d74ada3
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-01-04 20:23:12 +0800

    feat: create SkillContainer component

---
+++ src/lib/components/SkillContainer.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    type Props = {
+        name: string,
+        items: object[]
+    };
+
+    let {name, items = []} = $props();
+</script>
+
+<div>
+    <h3 class="h3">{name}</h3>
+</div>
--- src/lib/components/SkillSummary.svelte
+++ src/lib/components/SkillSummary.svelte
@@ -1,7 +1,11 @@
+<script>
+	import SkillContainer from "./SkillContainer.svelte";
+
+</script>
 <div class="flex prose dark:prose-invert w-full justify-center items-center bg-white dark:bg-slate-800 rounded-3xl max-w-none p-10">
     <div class="grid grid-cols-3 gap-10 justify-items-center prose-headings:my-4">
-        <div><h3>Languages</h3></div>
-        <div><h3>Libraries and Frameworks</h3></div>
-        <div><h3>Technologies</h3></div>
+        <SkillContainer name="Languages" />
+        <SkillContainer name="Libraries & Frameworks" />
+        <SkillContainer name="Technologies" />
     </div>
 </div>
