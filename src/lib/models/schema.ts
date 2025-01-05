dc1eb08d02cba80459627202a2310d390e92bacb
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-01-05 14:48:32 +0800

    feat: add frameworks and technologies to schema model

--- src/lib/models/schema.ts
+++ src/lib/models/schema.ts
@@ -4,13 +4,17 @@ import { Blog } from './blog';
 import { Tags } from './blog/tag';
 import { BlogPostsTags } from './blog/blog_posts_tags';
 import { Languages } from './work/languages';
+import { Frameworks } from './work/frameworks';
+import { Technologies } from './work/technologies';
 
 export const Schema = object({
 	blog: Blog,
 	blog_posts: BlogPosts,
 	blog_posts_tags: BlogPostsTags,
 	tags: Tags,
-	languages: Languages
+	languages: Languages,
+	frameworks: Frameworks,
+	technologies: Technologies,
 });
 
 export type Schema = InferOutput<typeof Schema>;
