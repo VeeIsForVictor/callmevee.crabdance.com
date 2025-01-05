09aa17bef53b9c028c41bb168de0410466d5a866
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-01-05 14:16:38 +0800

    feat: add languages model to schema

--- src/lib/models/schema.ts
+++ src/lib/models/schema.ts
@@ -3,12 +3,14 @@ import { BlogPosts } from './blog_post';
 import { Blog } from './blog';
 import { Tags } from './tag';
 import { BlogPostsTags } from './blog_posts_tags';
+import { Languages } from './languages';
 
 export const Schema = object({
 	blog: Blog,
 	blog_posts: BlogPosts,
 	blog_posts_tags: BlogPostsTags,
-	tags: Tags
+	tags: Tags,
+	languages: Languages
 });
 
 export type Schema = InferOutput<typeof Schema>;
