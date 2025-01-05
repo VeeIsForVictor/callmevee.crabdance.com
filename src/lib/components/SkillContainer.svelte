a65633620b1a908ed707c85d27effb4f8e6e85c7
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-01-05 15:33:42 +0800

    feat: add hover styling to skills

--- src/lib/components/SkillContainer.svelte
+++ src/lib/components/SkillContainer.svelte
@@ -18,7 +18,7 @@
     <h3 class="h3">{name}</h3>
     {#each items as item}
         {@const imageURL=`${PUBLIC_APIURL}/assets/${item.icon}`}
-        <figure class="inline-block m-2">
+        <figure class="inline-block m-2 hover:card hover:card-hover py-1 px-2">
             <img class="m-auto h-12 w-12" src={imageURL} height=48 width=48 alt={`${item.icon} logo`}>
             <figcaption class="text-center">{item.name}</figcaption>
         </figure>
