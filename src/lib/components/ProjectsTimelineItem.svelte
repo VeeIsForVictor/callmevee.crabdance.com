1cabd64140ef0b8d91821f1377d29edaeb1549e7
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-01-08 17:55:43 +0800

    fix: fix overly-large margins in timeline items

--- src/lib/components/ProjectsTimelineItem.svelte
+++ src/lib/components/ProjectsTimelineItem.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
 	import { Project } from "$lib/models/work/projects";
-	import { TimelineItem } from "flowbite-svelte";
-	import { description } from "valibot";
 
     interface Props {
         project: Project
     }
     
     let { project } : Props = $props();
+    let { name, description } = project;
     let project_date = new Date(project.project_date);
 
     let date = `${project_date.getMonth()} ${project_date.getFullYear()}`
+    console.log(description)
 </script>
 
-<article class="card p-8 w-full !max-w-none prose dark:prose-invert">
-    <h3 class="h3">{project.name}</h3>
-    <hr />
-    {project.description}
+<article class="card p-8 w-full !max-w-none prose dark:prose-invert prose-p:m-0">
+    <h3 class="h3">{name}</h3>
+    <hr class="!mb-4"/>
+    {@html description}
 </article>
