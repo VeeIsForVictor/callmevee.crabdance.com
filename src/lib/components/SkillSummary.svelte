a4285bdce21e312ebbb912473b9e35a4a37e39e6
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-01-08 17:39:24 +0800

    fix: adjust spacing in SkillSummary

--- src/lib/components/SkillSummary.svelte
+++ src/lib/components/SkillSummary.svelte
@@ -10,7 +10,7 @@
 
     let { languages, frameworks, technologies }: Props = $props();
 </script>
-<div class="flex prose dark:prose-invert w-full justify-center items-center bg-white dark:bg-slate-800 rounded-3xl max-w-none p-10 shadow-md">
+<div class="flex prose dark:prose-invert w-full justify-center items-center bg-white dark:bg-slate-800 rounded-3xl max-w-none p-6 shadow-md">
     <div class="grid grid-cols-3 gap-10 justify-items-center prose-headings:my-4">
         <SkillContainer name="Languages" items={languages}/>
         <SkillContainer name="Libraries & Frameworks" items={frameworks}/>
