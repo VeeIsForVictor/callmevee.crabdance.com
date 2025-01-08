ecd1fcf2c8000f4d7a58b59bede1d85c649f9a0b
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-01-08 17:46:06 +0800

    feat: add table of contents to ProjectsTimeline

--- src/lib/components/ProjectsTimeline.svelte
+++ src/lib/components/ProjectsTimeline.svelte
@@ -10,7 +10,13 @@
     let { projects }: Props = $props();
 </script>
 
-
-{#each projects as project}
-    <ProjectsTimelineItem {project}/>
-{/each}
+<div class="flex w-full space-x-8">
+    <div class="flex card w-80 p-4 sticky top-20">
+        <h5 class="h5">Table of Contents</h5>
+    </div>
+    <div class="space-y-4 w-full">
+        {#each projects as project}
+            <ProjectsTimelineItem {project}/>
+        {/each}
+    </div>
+</div>
