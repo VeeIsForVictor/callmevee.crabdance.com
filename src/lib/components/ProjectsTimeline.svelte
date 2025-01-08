f993852f624508665b954863da847ce3b32299dc
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-01-08 17:39:40 +0800

    feat: add ProjectTimeline component

---
+++ src/lib/components/ProjectsTimeline.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { Projects } from "$lib/models/work/projects";
+	import { Timeline } from "flowbite-svelte";
+	import ProjectsTimelineItem from "./ProjectsTimelineItem.svelte";
+
+    interface Props {
+        projects: Projects
+    }
+
+    let { projects }: Props = $props();
+</script>
+
+
+{#each projects as project}
+    <ProjectsTimelineItem {project}/>
+{/each}
---
+++ src/lib/components/ProjectsTimelineItem.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Project } from "$lib/models/work/projects";
+	import { TimelineItem } from "flowbite-svelte";
+	import { description } from "valibot";
+
+    interface Props {
+        project: Project
+    }
+    
+    let { project } : Props = $props();
+    let project_date = new Date(project.project_date);
+
+    let date = `${project_date.getMonth()} ${project_date.getFullYear()}`
+</script>
+
+<div class="card p-8">
+    <h3 class="h3">{project.name}</h3>
+    <p>{project.description}</p>
+</div>
