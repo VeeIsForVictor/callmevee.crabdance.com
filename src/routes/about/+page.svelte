e800cdba2a73a9826ca792c7cf104bfca98b4fcb
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-01-21 16:08:46 +0800

    feat: revise styling for about page content

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,7 +3,9 @@
     let { about } = data;
 </script>
 
-<div class="flex flex-col space-y-5 prose dark:prose-invert">
+<div class="h-full w-full flex flex-col justify-left items-center text-justify gap-16 py-20">
     <h1 class="h1">About</h1>
-    {@html about.about_text}
+    <div class="flex flex-col h-full lg:w-9/12 !max-w-none shadow-md p-8 bg-surface-100 dark:bg-surface-900 rounded-lg gap-4 prose dark:prose-invert prose-headings:my-2">
+        {@html about.about_text}
+    </div>
 </div>
