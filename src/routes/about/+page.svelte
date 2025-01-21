de5ea2ed1eeed5777bbe5c4605920f46297ef95d
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-01-21 17:25:25 +0800

    feat: add title to About page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,6 +3,10 @@
     let { about } = data;
 </script>
 
+<svelte:head>
+    <title>About</title>
+</svelte:head>
+
 <div class="h-full w-full flex flex-col justify-left items-center text-justify gap-16 py-20">
     <h1 class="h1">About</h1>
     <div class="flex flex-col h-full lg:w-9/12 !max-w-none shadow-md p-8 bg-surface-100 dark:bg-surface-900 rounded-lg gap-4 prose dark:prose-invert prose-headings:my-2">
